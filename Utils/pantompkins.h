e4a029841ae4cb3973e8064e0867909f9064a8a9
bhaumik <bhaumikjain6@gmail.com>
2019-05-14 01:58:07 +0530

    Add pantompkins.h

---
+++ Utils/pantompkins.h
@@ -0,0 +1,17 @@
+#include "MathUtils.h"
+/*
+  This function is dependent on the following functions:
+  -> mean();                     -> convolution();
+  -> subtractElement();          -> findPeaks();
+  -> MhrPtBpf();                 -> sortI();
+  -> absf();                     -> computeHeartRate();
+  -> divideElement();            -> moving_window_average();
+  -> findMax();
+  -> MhrPtDf();
+  -> squareArray();
+*/
+
+void pantompkinsMhr(const float*, const size_t, int* , size_t* );
+void pantompkinsFhr(const float *, size_t , int* , size_t*);
+
+void createNewQrs(float* , )
